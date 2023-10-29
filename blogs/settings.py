f4e7aee95c266809491629317091a917fe508e32
Kirti Rajput <119095266+KIRTI639@users.noreply.github.com>
2023-10-29 11:48:39 +0530

    Update settings.py

--- blogs/settings.py
+++ blogs/settings.py
@@ -130,8 +130,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'kirtirajput2023@gmail.com'
-EMAIL_HOST_PASSWORD = 'cbrcktxdexkdiloz'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 
 STATIC_ROOT = BASE_DIR /'productionfiles'
 STATIC_URL = 'static/'
